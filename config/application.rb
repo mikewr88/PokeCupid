eef36b94291f5a1a4428e3b72558bc8625b837a8
Michele Milota <michelemilota@Michele-Milotas-MacBook.local>
2016-04-29 16:33:36 -0700

    threw out public/assets

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module PokeCupid
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
